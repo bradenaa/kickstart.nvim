72300d4c2ce4deb650d1f955d28aed85e7e10cf7
guru245 <guru245@users.noreply.github.com>
2025-05-11 09:11:50 +0900

    feat: switch vim-sleuth for guess-indent.nvim (#1512)

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require('lazy').setup({
   'NMAC427/guess-indent.nvim',
 =======
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
